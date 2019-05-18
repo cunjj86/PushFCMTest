2ecc1231958a85b7c9d80941eb95586f6f1f28de
cunjj86 <ljgcunjj@naver.com>
2019-05-18 13:49:13 +0900

    Initial commit

---
+++ app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
@@ -0,0 +1,15 @@
+package com.tje.pushfcmtest.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyMessageService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        Log.d("새 토큰", s);
+    }
+}
