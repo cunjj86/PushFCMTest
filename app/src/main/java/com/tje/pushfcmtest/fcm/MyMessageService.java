a577d0bc762896992fe8229510c9a10c3bf59e9b
cunjj86 <ljgcunjj@naver.com>
2019-05-18 14:36:28 +0900

    앱이 켜져있을 때도 알림이 올 수 있게 조치

--- app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
+++ app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
@@ -1,9 +1,12 @@
 package com.tje.pushfcmtest.fcm;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -65,6 +68,46 @@ public class MyMessageService extends FirebaseMessagingService {
 //            안드로이드 버젼 코드가 O 보다 크다 : 오레오 이상의 최신버젼이다.
 //            알림 채널을 만들어서 해당 채널에 푸시알림을 던져주는 방식
 
+//            알림 채널을 생성. 중요도를 보통으로 생성
+            NotificationChannel channel = new NotificationChannel("app Name", "app Name", NotificationManager.IMPORTANCE_DEFAULT);
+//            해당 채널에 대한 설명문 == 앱 설정에 들어가면 나타남
+            channel.setDescription("푸시 알림 테스트용 채널입니다.");
+//            채널에서 조명 (카카오톡 조명 같은 기능) 을 사용한다.
+            channel.enableLights(true);
+//            색깔은 뭘로 할건지?
+            channel.setLightColor(Color.GREEN);
+
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{1, 1000});
+            channel.setSound(defaultNotiUri, null);
+
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+            notificationManager.createNotificationChannel(channel);
+
+            builder = new NotificationCompat.Builder(this, channel.getId());
+
+//            푸시알림 상태창에 뜨는 아이콘 설정
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+//            알림 제목 설정
+            builder.setContentTitle(title);
+//            알림 내용 설정
+            builder.setContentText(body);
+//            알림의 울리는 소리 설정
+            builder.setSound(defaultNotiUri);
+//            알림이 왔을때 울리는 진동의 패턴
+            builder.setVibrate(new long[]{1, 1000});
+//            자동 삭제 되도록 설정
+            builder.setAutoCancel(true);
+//            알림을 누르면 어디로 갈지 아까 만든 pendingIntent 활용 지정
+            builder.setContentIntent(pendingIntent);
+
+//            실제로 알림을 띄우는 부분
+
+//            id를 일반 숫자로 고정하면, 항상 같은 id가 대입 -> 여러번 알림이 오면 기존 알림을 덮어씀.
+//            만약 여러개의 알림을 모두 띄우고 싶다면 그때 그때 다른 숫자가 들어가도록 코딩.
+            notificationManager.notify(1, builder.build());
+
         }
         else {
 //            오레오 보다는 하위버젼이다. N 버젼 이하. ex) 누가, 마쉬멜로우, 롤리팝, 아이스크림샌드위치
@@ -81,7 +124,7 @@ public class MyMessageService extends FirebaseMessagingService {
 //            알림의 울리는 소리 설정
             builder.setSound(defaultNotiUri);
 //            알림이 왔을때 울리는 진동의 패턴
-            builder.setVibrate(new long[1, 1000]);
+            builder.setVibrate(new long[]{1, 1000});
 //            자동 삭제 되도록 설정
             builder.setAutoCancel(true);
 //            알림을 누르면 어디로 갈지 아까 만든 pendingIntent 활용 지정
